5136954d8ed8e081d18af0f337ac0c9d130e1c38
sayouuud <sayedelshazly2006@gmail.com>
2026-03-24 05:52:09 +0200

    final version inshaa allah

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,18 @@
 module.exports = {
     plugins: [
         ['@tailwindcss/postcss', {}],
+        ['postcss-preset-env', {
+            stage: 2,
+            minimumVendorImplementations: 0,
+            browsers: 'Chrome >= 80, Safari >= 13, iOS >= 13, Firefox >= 78, Samsung >= 13, UCAndroid >= 12',
+            features: {
+                'oklab-function': true,
+                'color-mix': true,
+                'nesting-rules': true,
+                'cascade-layers': true,
+                'custom-media-queries': true,
+                'media-query-ranges': true,
+            },
+        }],
     ],
 }
